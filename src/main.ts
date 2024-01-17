4063fe331cedb98a63aed743413d16a0ddbb65a1
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2024-01-17 17:30:47 +0100

    Enviroments Variables

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
+    .setTitle('Alua Backend')
+    .setDescription('Challenge backend for Alua')
     .setVersion('0.1')
     .build();
 
