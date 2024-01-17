7edc7d211f33585119de8821d8d16a0a566a435c
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2024-01-17 17:46:15 +0100

    Enviroments Variables

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Alua Backend')
     .setDescription('Challenge backend for Alua')
